873bf8c43b31c0f56123c1cf97cebb8fd7675fae
BurritoSpray <willpower1@live.ca>
2022-03-29 18:47:09 -0400

    initial commit
    First commit

---
+++ src/main/java/ez/clap/gestionetudian_aql/HelloController.java
@@ -0,0 +1,14 @@
+package ez.clap.gestionetudian_aql;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module ez.clap.gestionetudian_aql {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens ez.clap.gestionetudian_aql to javafx.fxml;
+    exports ez.clap.gestionetudian_aql;
+}
