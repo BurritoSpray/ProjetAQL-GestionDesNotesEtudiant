4ee6bff941c1d54760456a4666c2e0fe15bff2dc
William Goyette <willpower1@live.ca>
2022-03-30 14:43:43 -0400

    Integration du comportement des boutons sur l'onglet Etudiant

--- src/main/java/ez/clap/gestionetudiant_aql/controllers/MainWindowController.java
+++ src/main/java/ez/clap/gestionetudiant_aql/controllers/MainWindowController.java
@@ -6,20 +6,81 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainWindowController {
+
+    enum Action{
+        Create,
+        Edit,
+        Delete
+    }
+
+
+
     @FXML
-    private Button buttonAddStudent;
+    public Button buttonAddStudent, buttonEditStudent, buttonDeleteStudent;
+
+
 
     @FXML
     private void onButtonAddStudentClick() throws IOException {
-        Stage myStage = new Stage();
-        myStage.setTitle("Ajouter un etudiant");
-        Parent root = FXMLLoader.load(MainWindow.class.getResource("add-student-window.fxml"));
-        myStage.setScene(new Scene(root));
-        myStage.show();
+        Stage stage = setupStudentStage(Action.Create);
+        stage.show();
     }
+
+
+    @FXML
+    private void onButtonEditStudentClick() throws IOException {
+        Stage stage = setupStudentStage(Action.Edit);
+        stage.show();
+    }
+
+    @FXML
+    private void onButtonDeleteStudentClick() throws IOException {
+        Stage stage = setupStudentStage(Action.Delete);
+        stage.show();
+    }
+
+
+    private Stage setupStudentStage(Action action) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(MainWindow.class.getResource("manage-student-window.fxml"));
+        Parent root = loader.load();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setResizable(false);
+
+        switch(action){
+            // switch pour setup la fenetre
+            case Edit:{
+                stage.setTitle("Modifier l'étudiant");
+                ManageStudentWindow manageStudentWindow = loader.getController();
+                manageStudentWindow.loadUser("Test");
+                manageStudentWindow.buttonConfirm.setText("Modifier");
+                break;
+            }
+            case Create:{
+                stage.setTitle("Creer un étudiant");
+                break;
+            }
+            case Delete:{
+                stage.setTitle("Attention!");
+                loader = new FXMLLoader(MainWindow.class.getResource("delete-student-window.fxml"));
+                root = loader.load();
+                break;
+            }
+            default:{
+                loader = new FXMLLoader(getClass().getResource("manage-student-window.fxml"));
+                root = loader.load();
+            }
+        }
+
+        stage.setScene(new Scene(root));
+        return stage;
+    }
+
+
 }
