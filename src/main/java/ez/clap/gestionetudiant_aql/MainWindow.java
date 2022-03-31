fcc3456d36c21d4b5119a2839cd6a1a3977bcb47
William Goyette <willpower1@live.ca>
2022-03-31 15:03:09 -0400

    Modification de ma fonction test

--- src/main/java/ez/clap/gestionetudiant_aql/MainWindow.java
+++ src/main/java/ez/clap/gestionetudiant_aql/MainWindow.java
@@ -1,11 +1,13 @@
 package ez.clap.gestionetudiant_aql;
 
 import ez.clap.gestionetudiant_aql.controllers.MainWindowController;
+import ez.clap.gestionetudiant_aql.entities.Course;
 import ez.clap.gestionetudiant_aql.entities.Student;
 import ez.clap.gestionetudiant_aql.utilities.Data;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
@@ -43,7 +45,7 @@ public class MainWindow extends Application {
         mainWindowController.tableColumnFirstName.setCellValueFactory(new PropertyValueFactory<Student, String>("FirstName"));
         mainWindowController.tableColumnSecondName.setCellValueFactory(new PropertyValueFactory<Student, String>("SecondName"));
         mainWindowController.tableColumnNumber.setCellValueFactory(new PropertyValueFactory<Student, String>("StudentID"));
-        mainWindowController.tableColumnCourse.setCellValueFactory(new PropertyValueFactory<Student, ArrayList<String>>("CourseList"));
+        mainWindowController.tableColumnCourse.setCellValueFactory(new PropertyValueFactory<Student, ComboBox<String>>("CourseListAsComboBox"));
         tableView.getItems().addAll(Data.getStudentList());
 
         tableView.setOnMouseClicked(event -> {
@@ -64,7 +66,9 @@ public class MainWindow extends Application {
         Student s2 = new Student("a34324", "dsfsf", "2543174");
         Student s3 = new Student("as4sfdsgs", "dfdshvxvcvf", "2353657");
         Student s4 = new Student("asdddccgrs", "daaaa", "234399874");
-        ArrayList<Student> students = new ArrayList<>(Arrays.asList(s, s2, s3, s4));
+        Student s5 = new Student("Test", "test3342", "dsger", new ArrayList<>(Arrays.asList(new Course("MonCours", "12345", "54321"),
+                new Course("MonCours2", "42314", "12345633"))));
+        ArrayList<Student> students = new ArrayList<>(Arrays.asList(s, s2, s3, s4, s5));
         Data.setStudentList(students);
     }
 }
