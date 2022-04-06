a0b4ba91f59c9d8a33784f967b87009bb233e3cf
Jeremy Rodrigue <xobrys@gmail.com>
2022-04-05 23:11:17 -0400

    implémentation du controlleur pour l'interface de gestion des cours

---
+++ src/main/java/ez/clap/gestionetudiant_aql/controllers/ManageCourseController.java
@@ -0,0 +1,35 @@
+package ez.clap.gestionetudiant_aql.controllers;
+
+import ez.clap.gestionetudiant_aql.entities.Course;
+import ez.clap.gestionetudiant_aql.entities.Student;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class ManageCourseController {
+    @FXML
+    public Button buttonConfirm, buttonCancel;
+
+    @FXML
+    public TextField textFieldNumber, textFieldCode, textFieldTitle;
+
+    @FXML
+    private void onButtonConfirmClick(){
+
+    }
+
+    @FXML
+    private void onButtonCancelClick(){
+        Stage stage = (Stage) buttonCancel.getScene().getWindow();
+        stage.close();
+    }
+
+    public void loadCourse(Course course){
+        textFieldNumber.setText(course.getCourseNumber());
+        textFieldCode.setText(course.getCode());
+        textFieldTitle.setText(course.getTitle());
+    }
+
+}
