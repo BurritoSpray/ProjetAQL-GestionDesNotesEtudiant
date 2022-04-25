b509a0151c5664c53cb0f151830e9ef28d904ffa
William Goyette <willpower1@live.ca>
2022-04-24 23:44:27 -0400

    Reparation d'un bug majeur lors de la modification et la creation d'un etudiant avec des notes

--- src/main/java/ez/clap/gestionetudiant_aql/controllers/ManageStudentController.java
+++ src/main/java/ez/clap/gestionetudiant_aql/controllers/ManageStudentController.java
@@ -23,44 +23,51 @@ public class ManageStudentController {
     public ListView<CheckBox> listViewCourse;
 
     private Student selectedStudent;
+    private boolean isNewStudent;
     private MainWindowController mainWindowController;
 
 
-    private void loadStudent(){
+    private void loadStudent() {
         textFieldFirstName.setText(this.selectedStudent.getFirstName());
         textFieldSecondName.setText(this.selectedStudent.getSecondName());
         textFieldNumber.setText(this.selectedStudent.getStudentID());
     }
 
-    private void setupEvents(){
+    private void setupEvents() {
         this.buttonConfirm.setOnAction(actionEvent -> addStudentIfValid());
         this.buttonCancel.setOnAction(actionEvent -> closeWindow());
     }
 
-    private void loadCourseList(){
-        for(Course course : Data.getCourseList()){
+    private void loadCourseList() {
+        for (Course course : Data.getCourseList()) {
             CheckBox courseTitleCheckBox = new CheckBox();
             courseTitleCheckBox.setText(course.getTitle());
-            if(this.selectedStudent.getCourseList().contains(course)){
+            if (this.selectedStudent.getCourseList().contains(course)) {
                 courseTitleCheckBox.setSelected(true);
             }
             listViewCourse.getItems().add(courseTitleCheckBox);
         }
     }
 
-    public void loadData(MainWindowController mainWindowController, boolean newStudent){
+    public void loadData(MainWindowController mainWindowController, boolean newStudent) {
         this.mainWindowController = mainWindowController;
         this.selectedStudent = newStudent ? new Student() : mainWindowController.tableViewStudent.getSelectionModel().getSelectedItem();
+        this.isNewStudent = newStudent;
         loadStudent();
         loadCourseList();
         setupEvents();
     }
-    
-    private void addStudent(){
+
+    private void addStudent() {
         ArrayList<Course> studentCourseList = new ArrayList<>();
-        for(int i = 0; i < listViewCourse.getItems().size(); i++){
-            if(listViewCourse.getItems().get(i).isSelected()){
-                studentCourseList.add(Data.getCourseList().get(i));
+        if (!isNewStudent)
+            studentCourseList = fixCourseList();
+        else {
+            for (int i = 0; i < listViewCourse.getItems().size(); i++) {
+                if (listViewCourse.getItems().get(i).isSelected()) {
+                    Data.getCourseList().get(i).getGradeList().clear();
+                    studentCourseList.add(Data.getCourseList().get(i));
+                }
             }
         }
         Student student = new Student(this.textFieldFirstName.getText(),
@@ -68,10 +75,35 @@ public class ManageStudentController {
                 this.textFieldNumber.getText(), studentCourseList);
         Data.getStudentList().remove(selectedStudent);
         Data.getStudentList().add(student);
-        mainWindowController.tableViewStudent.refresh();
+        this.mainWindowController.tableViewStudent.refresh();
     }
-    
-    private void addStudentIfValid(){
+
+    private ArrayList<Course> fixCourseList() {
+        Student tempStudent = this.selectedStudent;
+        ArrayList<Course> newCourseList = new ArrayList<>();
+        for (int i = 0; i < this.listViewCourse.getItems().size(); i++) {
+            Course course = Data.getCourseList().get(i);
+            if(this.listViewCourse.getItems().get(i).isSelected()) {
+                boolean isAdded = false;
+                for (Course studentCourse : tempStudent.getCourseList()) {
+                    if (course.getCourseNumber().equals(studentCourse.getCourseNumber()) &&
+                            course.getCode().equals(studentCourse.getCode()) &&
+                            course.getTitle().equals(studentCourse.getTitle())) {
+                        newCourseList.add(studentCourse);
+                        tempStudent.getCourseList().remove(studentCourse);
+                        isAdded = true;
+                        break;
+                    }
+                }
+                if (!isAdded) {
+                    newCourseList.add(course);
+                }
+            }
+        }
+        return newCourseList;
+    }
+
+    private void addStudentIfValid() {
         if (isStudentFieldsValid()) {
             addStudent();
             closeWindow();
@@ -79,15 +111,15 @@ public class ManageStudentController {
             this.mainWindowController.showWarningPopup("Erreur", "Information manquante!", "OK");
         }
     }
-    
-    private boolean isStudentFieldsValid(){
+
+    private boolean isStudentFieldsValid() {
         return !this.textFieldFirstName.getText().isEmpty() &&
                 !this.textFieldSecondName.getText().isEmpty() &&
                 !this.textFieldNumber.getText().isEmpty();
     }
-    
-    private void closeWindow(){
-        ((Stage)this.buttonConfirm.getScene().getWindow()).close();
+
+    private void closeWindow() {
+        ((Stage) this.buttonConfirm.getScene().getWindow()).close();
     }
 
 
